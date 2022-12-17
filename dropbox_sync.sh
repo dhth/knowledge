132d69182b181f956393a509bceed156d2c160bd
Dhruv Thakur <dhruvt93@gmail.com>
2022-12-17 09:41:36 +0100

    Add dropbox sync helper commands

---
+++ dropbox_sync.sh
@@ -0,0 +1,8 @@
+xattr -w com.dropbox.ignored 1 /Users/dhruvthakur/Dropbox/knowledge/_layouts/node_modules
+xattr -w com.dropbox.ignored 1 /Users/dhruvthakur/Dropbox/knowledge/_layouts/public
+xattr -w com.dropbox.ignored 1 /Users/dhruvthakur/Dropbox/knowledge/_layouts/.cache
+xattr -w com.dropbox.ignored 1 /Users/dhruvthakur/Dropbox/knowledge/_layouts/yarn-error.log
+
+
+# To set the file/directory to no longer be removed
+# xattr -d com.dropbox.ignored /Users/dhruvthakur/Dropbox/knowledge/_layouts/.cache
